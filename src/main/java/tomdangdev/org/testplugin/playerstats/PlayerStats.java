b700382d5950427ede4c1fefbe8d374cbaa600e2
Kitslano828 <163047078+Kitslano828@users.noreply.github.com>
2024-05-13 17:59:47 -0700

    Update PlayerStats.java
    More work on PlayerStats

--- src/main/java/tomdangdev/org/testplugin/playerstats/PlayerStats.java
+++ src/main/java/tomdangdev/org/testplugin/playerstats/PlayerStats.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class PlayerStats {
 
-    private int damage, strength, speed, critChance,
+    private int damage, strength, speed, criticalChance,
                 mana, miningSpeed, miningFortune, farmingFortune,
                 lootFind;
 
@@ -13,7 +13,7 @@ public class PlayerStats {
         this.damage = 20;
         this.strength = 5;
         this.speed = 20;
-        this.critChance = 1;
+        this.criticalChance = 1;
         this.mana = 100;
         this.miningSpeed = 20;
         this.miningFortune = 0;
@@ -27,11 +27,22 @@ public class PlayerStats {
     }
     public int getStrength() { return strength; }
     public int getSpeed() { return speed; }
-    public int getCritChance() { return critChance; }
+    public int getCriticalChance() { return criticalChance; }
     public int getMana() { return mana; }
     public int getMiningSpeed() { return miningSpeed; }
     public int getMiningFortune() { return miningFortune; }
     public int getFarmingFortune() { return farmingFortune; }
     public int getLootFind() { return lootFind; }
+
+    // setting player stats
+    public void setDamage(int damage) {this.damage = damage;}
+    public void setStrength(int strength) {this.strength = strength;}
+    public void setSpeed(int speed) {this.speed = speed;}
+    public void setCriticalChance(int criticalChance) {this.criticalChance = criticalChance;}
+    public void setMana(int mana) {this.mana = mana;}
+    public void setMiningSpeed (int miningSpeed) {this.miningSpeed = miningSpeed;}
+    public void setMiningFortune (int miningFortune) {this.miningFortune = miningFortune;}
+    public void setFarmingFortune (int farmingFortune) {this.farmingFortune = farmingFortune;}
+    public void setLootFind (int lootFind) {this.lootFind = lootFind;}
 
 }
